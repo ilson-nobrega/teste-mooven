a7be4981cc5a39e6bbfdfb35f8f118ee65a1343d
Ilson Nobrega <ilson.nobrega@gmail.com>
2019-07-22 03:32:33 -0300

    Closes #9 - Agora sim finalizado. Tinha esquecido de subir a ação de login no Github nas páginas 404 e 403

--- src/app/@core/not-found/not-found.component.ts
+++ src/app/@core/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() {
+  }
+
+  signInWithGitHub($event): void {
+    // Removemos o evento default do link href para que execute a instrução que desejamos
+    $event.preventDefault();
+    // Executo o redirecionamento pro seviço de autenticação criado em NodeJS nas functions do Firebase
+    // Poderia ter sido construído com uma API diferente, onde a gente faria o request e a API retornasse o token
+    // Mas prezamos pela simplicidade e também pela mostragem das camadas de desenvolvimento envolvendo o NodeJS
+    this.document.location.href = 'https://us-central1-teste-mooven.cloudfunctions.net/login';
   }
 
 }
